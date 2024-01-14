9389a783deec18f533d3d7e531a25d26a52f45cb
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2024-01-14 15:51:06 +0500

    login, register endpoint complete

---
+++ lib/constant.dart
@@ -0,0 +1,13 @@
+//     url
+const baseURL = 'http://127.0.0.1:8000';
+const loginURL = '$baseURL/login';
+const registerURL = '$baseURL/register';
+const logoutURL = '$baseURL/logout';
+const userURL = '$baseURL/user';
+const postsURL = '$baseURL/posts';
+const commentsURL = '$baseURL/comments';
+
+// errors
+const serverError = 'server error';
+const unauthorizedError = 'unauthorized';
+const somethingwentwrongError = 'some thing went wrong';
---
+++ lib/services/user_services.dart
@@ -0,0 +1,105 @@
+import 'dart:convert';
+import 'dart:ffi';
+
+import 'package:blog_front_end/constant.dart';
+import 'package:blog_front_end/models/User.dart';
+import 'package:blog_front_end/models/api_responce.dart';
+import 'package:http/http.dart ' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+// fetch login api
+Future<ApiResponce> login(String email, String password) async {
+  ApiResponce apiResponce = ApiResponce();
+  try {
+    final responce = await http.post(Uri.parse(loginURL),
+        headers: {'Accept': 'application/json'},
+        body: {'email': email, 'password': password});
+    switch (responce.statusCode) {
+      case 200:
+        apiResponce.data = User.fromJson(jsonDecode(responce.body));
+        break;
+      case 422:
+        final errors = jsonDecode(responce.body)['errors'];
+        apiResponce.error = errors[errors.keys.elementAt(0)][0];
+        break;
+      case 403:
+        apiResponce.error = jsonDecode(responce.body)['msg'];
+        break;
+      default:
+        apiResponce.error = somethingwentwrongError;
+        break;
+    }
+  } catch (e) {
+    apiResponce.error = serverError;
+  }
+  return apiResponce;
+}
+
+// fetch register user api
+Future<ApiResponce> register(String name, String email, String password) async {
+  ApiResponce apiResponce = ApiResponce();
+  try {
+    final responce = await http.post(Uri.parse(registerURL), headers: {
+      'Accept': 'application/json'
+    }, body: {
+      'name': name,
+      'email': email,
+      'password': password,
+      'password_confirmation': password
+    });
+    switch (responce.statusCode) {
+      case 200:
+        apiResponce.data = User.fromJson(jsonDecode(responce.body));
+        break;
+      case 422:
+        final errors = jsonDecode(responce.body)['errors'];
+        apiResponce.error = errors[errors.keys.elementAt(0)][0];
+        break;
+      case 403:
+        apiResponce.error = jsonDecode(responce.body)['msg'];
+        break;
+      default:
+        apiResponce.error = somethingwentwrongError;
+        break;
+    }
+  } catch (e) {
+    apiResponce.error = serverError;
+  }
+  return apiResponce;
+}
+
+// get all user api
+Future<ApiResponce> getUserDetail() async {
+  ApiResponce apiResponce = ApiResponce();
+  try {
+    String token = await getToken();
+    final responce = await http.post(Uri.parse(registerURL), headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token'
+    });
+    switch (responce.statusCode) {
+      case 200:
+        apiResponce.data = User.fromJson(jsonDecode(responce.body));
+        break;
+      case 422:
+        final errors = jsonDecode(responce.body)['errors'];
+        apiResponce.error = errors[errors.keys.elementAt(0)][0];
+        break;
+      case 403:
+        apiResponce.error = jsonDecode(responce.body)['msg'];
+        break;
+      default:
+        apiResponce.error = somethingwentwrongError;
+        break;
+    }
+  } catch (e) {
+    apiResponce.error = serverError;
+  }
+  return apiResponce;
+}
+
+// get token from shared preferences
+Future<String> getToken() async {
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  return pref.getString('token') ?? '';
+}
